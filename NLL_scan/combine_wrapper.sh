1e26d24a30cc6d890e5168bfebd0fb421dc70c69
green-cabbage <hyeonseo_yun@hotmail.com>
2025-08-19 13:42:53 -0400

    scale up with npoints 200

--- NLL_scan/combine_wrapper.sh
+++ NLL_scan/combine_wrapper.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
-# text2workspace.py -m 125 datacard_comb_sig_all_ggh.txt 
-# for index_val in {0..6}
-# # for index_val in {0..1}
-# do
-#   echo "Running combine with pdf_index_ggh=${index_val}"
-#   combine -M MultiDimFit -d datacard_comb_sig_all_ggh.txt \
-#     --algo grid \
-#     --setParameterRanges r=-1,5 \
-#     --cminDefaultMinimizerStrategy 0 \
-#     --saveNLL \
-#     -n fixed_pdf_${index_val} \
-#     -m 125 \
-#     --setParameters pdf_index_ggh=${index_val} \
-#     --freezeParameters pdf_index_ggh \
-#     --X-rtd REMOVE_CONSTANT_ZERO_POINT=1 \
-#     --points 10 # 20
-# done
+# npts="10"
+npts="200"
+text2workspace.py -m 125 datacard_comb_sig_all_ggh.txt 
+for index_val in {0..6}
+# for index_val in {0..1}
+do
+  echo "Running combine with pdf_index_ggh=${index_val}"
+  combine -M MultiDimFit -d datacard_comb_sig_all_ggh.txt \
+    --algo grid \
+    --setParameterRanges r=-2,6 \
+    --cminDefaultMinimizerStrategy 0 \
+    --saveNLL \
+    -n fixed_pdf_${index_val} \
+    -m 125 \
+    --setParameters pdf_index_ggh=${index_val} \
+    --freezeParameters pdf_index_ggh \
+    --X-rtd REMOVE_CONSTANT_ZERO_POINT=1 \
+    --points ${npts} 
+done
 
 index_val="Envelope"
 echo "Running combine with pdf_index_ggh=${index_val}"
 combine -M MultiDimFit -d datacard_comb_sig_all_ggh.txt \
     --algo grid \
-    --setParameterRanges r=-1,5 \
+    --setParameterRanges r=-2,6 \
     --cminDefaultMinimizerStrategy 0 \
     --cminRunAllDiscreteCombinations \
     --saveNLL \
@@ -30,4 +32,4 @@ combine -M MultiDimFit -d datacard_comb_sig_all_ggh.txt \
     -m 125 \
     --setParameters pdf_index_ggh=-1 \
     --X-rtd REMOVE_CONSTANT_ZERO_POINT=1 \
-    --points 10 # 20
+    --points ${npts} 
